72d6b97470befcb02a2af1087e8e702dfb5dd2b1
rjmendezl <00103719@uca.edu.sv>
2020-04-19 20:18:56 -0600

    Modify: class Documento has been modified

--- parcial01_GrupoDinamita/src/com/RJML/x00103719/Documento.java
+++ parcial01_GrupoDinamita/src/com/RJML/x00103719/Documento.java
@@ -16,4 +16,12 @@ public class Documento{
     public String getNumero() {
         return numero;
     }
+
+    @Override
+    public String toString() {
+        return "Documento{" +
+                "nombre='" + nombre + '\'' +
+                ", numero='" + numero + '\'' +
+                '}';
+    }
 }
