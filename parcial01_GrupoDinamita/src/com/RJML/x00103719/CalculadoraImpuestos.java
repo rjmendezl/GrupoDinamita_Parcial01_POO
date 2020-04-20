cb26b846f2d038ea84e44bc6b51dcb9b4944ed5e
rjmendezl <00103719@uca.edu.sv>
2020-04-19 20:15:36 -0600

    Modify: class CalculadoraImpuestos has been modified

--- parcial01_GrupoDinamita/src/com/RJML/x00103719/CalculadoraImpuestos.java
+++ parcial01_GrupoDinamita/src/com/RJML/x00103719/CalculadoraImpuestos.java
@@ -2,40 +2,35 @@ package com.RJML.x00103719;
 
 public final class CalculadoraImpuestos {
     private static double totalRenta = 0, totalISSS = 0, totalAFP = 0;
-
-    public CalculadoraImpuestos() {
+    private CalculadoraImpuestos() {
     }
-
-    public static double calcularPago(Empleado contratado) {
-        System.out.println("-------Calculando Pago--------");
-        double isss = 0, PagoEmpleado = 0, afp = 0, Renta = 0;
-        double Restante = 0, salarioLiq = contratado.getSalario();
-
-        if (contratado instanceof PlazaFija) {
+    public static double calcularPago(Empleado empleado) {
+        double isss = 0, pagoEmpleado, afp = 0, renta = 0;
+        double restante, salarioLiq = empleado.getSalario();
+        if (empleado instanceof PlazaFija) {
             afp = 0.0625 * salarioLiq;
             isss = 0.03 * salarioLiq;
-            Restante = salarioLiq - afp - isss;
-            if (Restante >= 0.01 && Restante <= 472.00)
-                Renta = 0; //probar con = Restante
-            else if (Restante >= 472.01 && Restante <= 895.24)
-                Renta = (0.1 * (Restante - 472.00) + 17.67);
-            else if (Restante >= 895.25 && Restante <= 2038.10)
-                Renta = (0.2 * (Restante - 895.24) + 60);
-            else if (Restante >= 2038.11) {
-                Renta = (0.3 * (Restante - 2038.10) + 288.57);
+            restante = salarioLiq - afp - isss;
+            if (restante >= 0.01 && restante <= 472.00)
+                renta = 0;
+            else if (restante >= 472.01 && restante <= 895.24)
+                renta = (0.1 * (restante - 472.00) + 17.67);
+            else if (restante >= 895.25 && restante <= 2038.10)
+                renta = (0.2 * (restante - 895.24) + 60);
+            else if (restante >= 2038.11) {
+                renta = (0.3 * (restante - 2038.10) + 288.57);
             }
-
-        }else
-            Renta = 0.1 * salarioLiq;
-        PagoEmpleado = salarioLiq - Renta;
-        totalRenta += Renta;
+            pagoEmpleado = restante - renta;
+        }else{
+            renta = 0.1 * salarioLiq;
+            pagoEmpleado = salarioLiq - renta;
+        }
+        totalRenta += renta;
         totalAFP += afp;
         totalISSS += isss;
-
-        return PagoEmpleado;
+        return pagoEmpleado;
     }
-
     public static String mostrarTotales(){
-        return ("Total AFP: " + totalAFP + " Total ISSS: " + totalISSS + " Total Renta: " + totalRenta);
+        return ("\nTotal AFP: "+String.format("%.2f",totalAFP) +"\nTotal ISSS: "+String.format("%.2f",totalISSS)+"\nTotal Renta: "+String.format("%.2f",totalRenta));
     }
 }
